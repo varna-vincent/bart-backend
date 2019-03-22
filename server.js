1055ec3edc55c491d56b4689276c73f9e52a754c
varna-vincent <vincent.varna@gmail.com>
2019-03-22 10:00:30 -0700

    Error conditions

--- server.js
+++ server.js
@@ -23,29 +23,31 @@ app.get("/stations", (req, res, next) => {
 });
 
 app.get("/trips", (req, res, next) => {
+	if(req.query.source != "null" && req.query.destination != "null") {
+		Request.get("http://api.bart.gov/api/sched.aspx?cmd=depart&orig=" + req.query.source + "&dest=" + req.query.destination + "&before=0&after=4&date=now&key=QEMQ-5X6J-9JTT-DWE9&json=y", (error, response, body) => {
+		    if(error) {
+		        return console.dir(error);
+		    }
+		    console.dir(JSON.parse(body));
 
-	Request.get("http://api.bart.gov/api/sched.aspx?cmd=depart&orig=" + req.query.source + "&dest=" + req.query.destination + "&before=0&after=4&date=now&key=QEMQ-5X6J-9JTT-DWE9&json=y", (error, response, body) => {
-	    if(error) {
-	        return console.dir(error);
-	    }
-	    console.dir(JSON.parse(body));
+		    let root = JSON.parse(body).root;
+		    let schedule = root.schedule;
+		    schedule.origin = root.origin;
+		    schedule.destination = root.destination;
 
-	    let root = JSON.parse(body).root;
-	    let schedule = root.schedule;
-	    schedule.origin = root.origin;
-	    schedule.destination = root.destination;
-
- 		res.json(schedule);
-	});
+	 		res.json(schedule);
+		});
+	}
 });
 
 app.get("/station", (req, res, next) => {
-
-	Request.get("http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=" + req.query.source + "&key=MW9S-E7SL-26DU-VV8V&json=y", (error, response, body) => {
-	    if(error) {
-	        return console.dir(error);
-	    }
-	    console.dir(JSON.parse(body));
- 		res.json(JSON.parse(body).root.stations.station);
-	});
+	if(req.query.source != "null") {
+		Request.get("http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=" + req.query.source + "&key=MW9S-E7SL-26DU-VV8V&json=y", (error, response, body) => {
+		    if(error) {
+		        return console.dir(error);
+		    }
+		    console.dir(JSON.parse(body));
+	 		res.json(JSON.parse(body).root.stations.station);
+		});
+	}
 });
